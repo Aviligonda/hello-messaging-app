9699b75b51401b6cffb6cd114d67f74d313d389c
aviligonda <aviligondasreenivasulu6086@gmail.com>
2022-08-05 07:40:46 +0530

    ADDED : Pass the name as param get the message

--- main/java/com/bridgelabz/hellomessagingapp/controller/HelloMessagingAppController.java
+++ main/java/com/bridgelabz/hellomessagingapp/controller/HelloMessagingAppController.java
@@ -2,6 +2,7 @@ package com.bridgelabz.hellomessagingapp.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,5 +11,9 @@ public class HelloMessagingAppController {
     @GetMapping("/getmessage")
     public String hello() {
         return "Hello";
+    }
+    @GetMapping("/query")
+    public String queryParam(@RequestParam String name){
+        return "Hello "+name+" from BridgeLabz";
     }
 }
