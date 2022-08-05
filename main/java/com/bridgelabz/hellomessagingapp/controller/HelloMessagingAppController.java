86073a0f71b663491701fe6e3690c7058ff37282
aviligonda <aviligondasreenivasulu6086@gmail.com>
2022-08-05 12:15:07 +0530

    ADDED : adding the put method and pathVarialble method

--- main/java/com/bridgelabz/hellomessagingapp/controller/HelloMessagingAppController.java
+++ main/java/com/bridgelabz/hellomessagingapp/controller/HelloMessagingAppController.java
@@ -6,24 +6,45 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/hello")
 public class HelloMessagingAppController {
+    // Url : http://localhost:8080/hello/getmessage
     @GetMapping("/getmessage")
     public String hello() {
         return "Hello";
     }
 
+    //Url : http://localhost:8080/hello/query?name=srinivas
     @GetMapping("/query")
     public String queryParam(@RequestParam String name) {
         return "Hello " + name + " from BridgeLabz";
     }
 
+    //Url : http://localhost:8080/hello/adduser
     @PostMapping("/adduser")
     public String adduser(@RequestBody User user) {
         return user.toString();
     }
 
-    @PostMapping("adduserwithparams")
+    //Url : http://localhost:8080/hello/adduserwithparams
+    @PostMapping("/adduserwithparams")
     public String addUserWithParams(@RequestParam String firstName, @RequestParam String lastName) {
         User user = new User();
+        user.setFirstName(firstName);
+        user.setLastName(lastName);
+        return user.toString();
+    }
+
+    // Url : http://localhost:8080/hello/path/Srinivas
+    @GetMapping("/path/{name}")
+    public String name(@PathVariable String name) {
+        return name;
+    }
+
+    //Url : http://localhost:8080/hello/updated?firstName=Sai&lastName=Kiran
+    @PutMapping("/updated")
+    public String update(@RequestParam String firstName, @RequestParam String lastName) {
+        User user = new User();
+        user.setFirstName("Aviligonda");
+        user.setLastName("Srinivas");
         user.setFirstName(firstName);
         user.setLastName(lastName);
         return user.toString();
